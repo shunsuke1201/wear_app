6b6feb939d82bac64fbd0a8b0565bdd6b88f16cb
Ubuntu <ubuntu@ip-172-31-2-115.ap-northeast-1.compute.internal>
2023-03-09 08:59:02 +0000

    　フォローボタン、人数非同期化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,22 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:create, :destroy]
+  
   def create
     following = current_user.relationships.build(follower_id: params[:user_id])
     following.save
-    redirect_to request.referrer || root_path
+    
   end
   
   def destroy
     following = current_user.relationships.find_by(follower_id: params[:user_id])
     following.destroy
-    redirect_to request.referrer || root_path
+    
+  end
+  
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
