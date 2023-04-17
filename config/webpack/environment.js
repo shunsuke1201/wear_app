1fd6910327586cffd7c3653933255178893862e6
Ubuntu <ubuntu@ip-172-31-2-115.ap-northeast-1.compute.internal>
2023-04-17 02:01:34 +0000

    config/webpack/environment.js編集

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,24 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  }
+};
+
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
+environment.config.merge(customConfig);
 
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
