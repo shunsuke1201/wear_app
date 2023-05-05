9c0af82b6231fd7fa04885d727c70aac53ee4ff5
Ubuntu <ubuntu@ip-172-31-2-115.ap-northeast-1.compute.internal>
2023-05-05 06:34:30 +0000

    モデルテスト追加

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーションテスト' do
+    it 'Userとの関連付けがされていること' do
+      expect(Genre.reflect_on_association(:users).macro).to eq :has_many
+    end
+
+    it 'UserGenreとの関連付けがされていること' do
+      expect(Genre.reflect_on_association(:user_genres).macro).to eq :has_many
+    end
+  end
 end
--- spec/models/user_genre_spec.rb
+++ spec/models/user_genre_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UserGenre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーションテスト' do
+    it 'Userとの関連付けがされていること' do
+      expect(UserGenre.reflect_on_association(:user).macro).to eq :belongs_to
+    end
+
+    it 'UserGenreとの関連付けがされていること' do
+      expect(UserGenre.reflect_on_association(:genre).macro).to eq :belongs_to
+    end
+  end
 end
